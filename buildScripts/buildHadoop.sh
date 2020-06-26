e51c078cdc625c52405ae3c1b0997f401c4b5564
ajay-ops <67255421+ajay-ops@users.noreply.github.com>
2020-06-26 17:32:23 +0530

    Update buildHadoop.sh

--- buildScripts/buildHadoop.sh
+++ buildScripts/buildHadoop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find . -name pom.xml -exec sed -i -e 's/<require.snappy>false<\/require.snappy>/<require.snappy>true<\/require.snappy>/g' {} \;
-mvn package -Pdist,native -Dyarn.arg="install --offline" -Dmaven.test.skip=true -Drequire.snappy -Dmaven.javadoc.skip=true -Dhbase.profile=2.0 -U -X 
+mvn package -Pdist,native -Dyarn.arg="install --offline" -DskipTests -Drequire.snappy -Dmaven.javadoc.skip=true -Dhbase.profile=2.0 -U -X
